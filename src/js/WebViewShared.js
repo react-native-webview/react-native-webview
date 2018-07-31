be9c217dfea4c8ac08d000a49f672f7f78977c81
Jamon Holmgren <jamon@infinite.red>
2018-07-30 20:21:48 -0700

    Initial

---
+++ src/js/WebViewShared.js
@@ -0,0 +1,26 @@
+/**
+ * Copyright (c) 2018-present, Infinite Red, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @format
+ * @flow
+ */
+
+'use strict';
+
+const escapeStringRegexp = require('escape-string-regexp');
+
+const WebViewShared = {
+  defaultOriginWhitelist: ['http://*', 'https://*'],
+  extractOrigin: (url) => {
+    const result = /^[A-Za-z0-9]+:(\/\/)?[^/]*/.exec(url);
+    return result === null ? null : result[0];
+  },
+  originWhitelistToRegex: (originWhitelist) => {
+    return escapeStringRegexp(originWhitelist).replace(/\\\*/g, '.*');
+  },
+};
+
+module.exports = WebViewShared;
