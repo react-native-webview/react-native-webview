ad40610c333470fd5e967b5fb622e22cd051d7c1
KaFai Choi <kafaicoder@gmail.com>
2018-11-25 17:58:52 +0700

    fix tsconfig and export WebView in index

---
+++ index.js
@@ -0,0 +1,3 @@
+import WebView from "./WebView";
+
+export { WebView };
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,2 @@
-import IOSWebView from "./WebView.ios"; 
-import AndroidWebView from "./WebView.android"; 
-
-declare var _test1: typeof IOSWebView;
-declare var _test2: typeof AndroidWebView;
-console.log('_test1', _test1)
-console.log('_test2', _test2)
-
-/// export to get the shape of the module
-export default { WebView: IOSWebView}
+export * from './types/WebViewTypes'
+export * from './WebView.ios'
