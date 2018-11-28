7f22f47b99426518af1c996192f19c1f4ac1443e
Wijnand Warren <god@ansuz.nl>
2018-11-28 14:39:07 +0100

    Use AndroidX instead of Support Library
    [Ticket: #176]

--- android/src/main/java/com/reactnativecommunity/webview/RNCWebViewFileProvider.java
+++ android/src/main/java/com/reactnativecommunity/webview/RNCWebViewFileProvider.java
@@ -1,6 +1,7 @@
 package com.reactnativecommunity.webview;
 
-import android.support.v4.content.FileProvider;
+
+import androidx.core.content.FileProvider;
 
 /**
  * Providing a custom {@code FileProvider} prevents manifest {@code <provider>} name collisions.
--- android/src/main/java/com/reactnativecommunity/webview/RNCWebViewModule.java
+++ android/src/main/java/com/reactnativecommunity/webview/RNCWebViewModule.java
@@ -8,8 +8,6 @@ import android.os.Build;
 import android.os.Environment;
 import android.os.Parcelable;
 import android.provider.MediaStore;
-import android.support.annotation.RequiresApi;
-import android.support.v4.content.FileProvider;
 import android.util.Log;
 import android.webkit.ValueCallback;
 import android.webkit.WebChromeClient;
@@ -23,6 +21,9 @@ import com.facebook.react.bridge.ReactMethod;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+
+import androidx.annotation.RequiresApi;
+import androidx.core.content.FileProvider;
 
 import static android.app.Activity.RESULT_OK;
 
