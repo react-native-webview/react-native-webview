da62911d953f14f0e07eb8202b53497de4fdf605
Paul Xu <px@horizongames.net>
2019-01-02 12:08:10 -0500

    D

--- ios/RNCWKWebViewManager.h
+++ ios/RNCWKWebViewManager.h
@@ -5,8 +5,7 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-//#import <React/RCTViewManager.h>
-#import <React>
+#import <React/RCTViewManager.h>
 
 @interface RNCWKWebViewManager : RCTViewManager
 @end
