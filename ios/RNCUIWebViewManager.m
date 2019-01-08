3fcbe00a160139cab33a5a17a0bc8c744e2144f7
meiqi1992 <378535936@qq.com>
2019-01-08 08:07:55 +0800

    Update RNCUIWebViewManager.m
    onscroll block

--- ios/RNCUIWebViewManager.m
+++ ios/RNCUIWebViewManager.m
@@ -42,7 +42,7 @@ RCT_REMAP_VIEW_PROPERTY(allowsInlineMediaPlayback, _webView.allowsInlineMediaPla
 RCT_REMAP_VIEW_PROPERTY(mediaPlaybackRequiresUserAction, _webView.mediaPlaybackRequiresUserAction, BOOL)
 RCT_REMAP_VIEW_PROPERTY(dataDetectorTypes, _webView.dataDetectorTypes, UIDataDetectorTypes)
 
-//添加
+//onScroll Block
 RCT_EXPORT_VIEW_PROPERTY(onScroll, RCTDirectEventBlock)
 
 
