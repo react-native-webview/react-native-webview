a193a112f144ec460e462c7674fa2613f09dc2ae
Suhan Moon <lunyx@me.com>
2019-01-13 23:15:40 +0900

    Update RNCWKWebView.m

--- ios/RNCWKWebView.m
+++ ios/RNCWKWebView.m
@@ -113,6 +113,8 @@ static NSString *const MessageHanderName = @"ReactNative";
     }
 #endif
 
+    [self keyboardDisplayDoesNotRequireUserAction];
+    
     [self addSubview:_webView];
     [self setHideKeyboardAccessoryView: _savedHideKeyboardAccessoryView];
     [self visitSource];
@@ -121,6 +123,29 @@ static NSString *const MessageHanderName = @"ReactNative";
 
 - (void)scrollViewDidScroll:(UIScrollView *)scrollView {
     if (_scrollEnabled == NO) scrollView.contentOffset = CGPointZero;
+}
+
+- (void) keyboardDisplayDoesNotRequireUserAction {
+    Class class = NSClassFromString(@"WKContentView");
+    NSOperatingSystemVersion iOS_11_3_0 = (NSOperatingSystemVersion){11, 3, 0};
+    
+    if ([[NSProcessInfo processInfo] isOperatingSystemAtLeastVersion: iOS_11_3_0]) {
+        SEL selector = sel_getUid("_startAssistingNode:userIsInteracting:blurPreviousNode:changingActivityState:userObject:");
+        Method method = class_getInstanceMethod(class, selector);
+        IMP original = method_getImplementation(method);
+        IMP override = imp_implementationWithBlock(^void(id me, void* arg0, BOOL arg1, BOOL arg2, BOOL arg3, id arg4) {
+            ((void (*)(id, SEL, void*, BOOL, BOOL, BOOL, id))original)(me, selector, arg0, TRUE, arg2, arg3, arg4);
+        });
+        method_setImplementation(method, override);
+    } else {
+        SEL selector = sel_getUid("_startAssistingNode:userIsInteracting:blurPreviousNode:userObject:");
+        Method method = class_getInstanceMethod(class, selector);
+        IMP original = method_getImplementation(method);
+        IMP override = imp_implementationWithBlock(^void(id me, void* arg0, BOOL arg1, BOOL arg2, id arg3) {
+            ((void (*)(id, SEL, void*, BOOL, BOOL, id))original)(me, selector, arg0, TRUE, arg2, arg3);
+        });
+        method_setImplementation(method, override);
+    }
 }
 
 - (void)removeFromSuperview
