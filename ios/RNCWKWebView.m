3d4f2518f274b5c0643e52d1ce9d0285a98a3f22
Benjos Antony <benjosantony@thoughtworks.com>
2019-01-24 16:08:59 +0800

    fix(WKWebView):  Allow _target links to be opened up in the same way as UIWebView
    fixes #139

--- ios/RNCWKWebView.m
+++ ios/RNCWKWebView.m
@@ -73,6 +73,17 @@ static NSString *const MessageHanderName = @"ReactNative";
   return self;
 }
 
+/**
+ * See https://stackoverflow.com/questions/25713069/why-is-wkwebview-not-opening-links-with-target-blank/25853806#25853806 for details.
+ */
+- (WKWebView *)webView:(WKWebView *)webView createWebViewWithConfiguration:(WKWebViewConfiguration *)configuration forNavigationAction:(WKNavigationAction *)navigationAction windowFeatures:(WKWindowFeatures *)windowFeatures
+{
+  if (!navigationAction.targetFrame.isMainFrame) {
+    [webView loadRequest:navigationAction.request];
+  }
+  return nil;
+}
+
 - (void)didMoveToWindow
 {
   if (self.window != nil && _webView == nil) {
