bee15de5526cdae34b407aa83f3fa4627f502954
Thibault Malbranche <thibault.malbranche@epitech.eu>
2019-03-20 11:36:58 +0000

    last fixes

--- src/WebViewShared.ts
+++ src/WebViewShared.ts
@@ -1,11 +1,3 @@
-/**
- * Copyright (c) 2015-present, Facebook, Inc.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- *
- */
-
 import escapeStringRegexp from 'escape-string-regexp';
 import { Linking, UIManager as NotTypedUIManager } from 'react-native';
 import {
