b904ee8cc82993a514e390e7b5e3f78c83f98824
Chet Corcos <ccorcos@gmail.com>
2019-08-21 12:41:19 -0700

    DispatchEvent on window to be consistent with iOS

--- android/src/main/java/com/reactnativecommunity/webview/RNCWebViewManager.java
+++ android/src/main/java/com/reactnativecommunity/webview/RNCWebViewManager.java
@@ -648,16 +648,9 @@ public class RNCWebViewManager extends SimpleViewManager<WebView> {
           RNCWebView reactWebView = (RNCWebView) root;
           JSONObject eventInitDict = new JSONObject();
           eventInitDict.put("data", args.getString(0));
+          // Make sure this is the same as iOS!
           reactWebView.evaluateJavascriptWithFallback("(function () {" +
-            "var event;" +
-            "var data = " + eventInitDict.toString() + ";" +
-            "try {" +
-            "event = new MessageEvent('message', data);" +
-            "} catch (e) {" +
-            "event = document.createEvent('MessageEvent');" +
-            "event.initMessageEvent('message', true, true, data.data, data.origin, data.lastEventId, data.source);" +
-            "}" +
-            "document.dispatchEvent(event);" +
+            "window.dispatchEvent(new MessageEvent('message', " + eventInitDict.toString() + "));" +
             "})();");
         } catch (JSONException e) {
           throw new RuntimeException(e);
