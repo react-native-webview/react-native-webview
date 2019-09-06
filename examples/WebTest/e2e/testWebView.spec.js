811f606420a3d1591f9c6e0cdb0efdb8f9a30ec4
Jamon Holmgren <jamon@infinite.red>
2019-09-06 16:49:29 +0200

    Upgraded RN in example app, detox changes

--- examples/WebTest/e2e/testWebView.spec.js
+++ examples/WebTest/e2e/testWebView.spec.js
@@ -1,6 +1,21 @@
+import {Platform} from 'react-native';
+
 describe('WebView basic test', () => {
   beforeEach(async () => {
     await device.launchApp({newInstance: true});
+  });
+
+  it('should not have a redbox visible', async () => {
+    const platform = Platform.select({
+      ios: 'RCTRedBoxWindow',
+      android: undefined, // TODO -- need to figure out what RedBox is on Android
+    });
+    if (platform) {
+      expect(element(by.type(platform))).toNotExist();
+    }
+
+    // so there's at least one assertion
+    expect(true).toBe(true);
   });
 
   it('should have a webview', async () => {
