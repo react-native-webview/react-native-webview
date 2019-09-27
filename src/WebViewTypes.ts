ab16895ebd0455890f8b519c78aa68daafc45f9c
Borut Balazek <bobalazek124@gmail.com>
2019-09-27 10:21:06 +0200

    Update WebViewTypes.ts

--- src/WebViewTypes.ts
+++ src/WebViewTypes.ts
@@ -483,6 +483,15 @@ export interface AndroidWebViewProps extends WebViewSharedProps {
    */
   geolocationEnabled?: boolean;
 
+  
+  /**
+   * Boolean that sets whether JavaScript running in the context of a file 
+   * scheme URL should be allowed to access content from other file scheme URLs. 
+   * Including accessing content from other file scheme URLs
+   * @platform android
+   */
+  allowFileAccessFromFileURLs?: boolean;
+  
   /**
    * Boolean that sets whether JavaScript running in the context of a file
    * scheme URL should be allowed to access content from any origin.
