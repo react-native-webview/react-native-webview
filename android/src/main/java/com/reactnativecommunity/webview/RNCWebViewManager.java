cb427b251492d7ba6b58070ebadb750835f576a9
Visakeswaran <vishu@cartoonmango.com>
2020-01-12 22:02:54 +0530

    [Add] - try removing mOriginWhitelist

--- android/src/main/java/com/reactnativecommunity/webview/RNCWebViewManager.java
+++ android/src/main/java/com/reactnativecommunity/webview/RNCWebViewManager.java
@@ -147,7 +147,7 @@ public class RNCWebViewManager extends SimpleViewManager<WebView> {
   protected boolean mAllowsFullscreenVideo = false;
   protected @Nullable String mUserAgent = null;
   protected @Nullable String mUserAgentWithApplicationName = null;
-  protected @Nullable List<Pattern> mOriginWhitelist;
+  // protected @Nullable List<Pattern> mOriginWhitelist;
 
   public RNCWebViewManager() {
     mWebViewConfig = new WebViewConfig() {
@@ -769,9 +769,9 @@ public class RNCWebViewManager extends SimpleViewManager<WebView> {
         }
       }
 
-      if (mOriginWhitelist != null && shouldHandleURL(mOriginWhitelist, url)) {
-        return false;
-      }
+      // if (mOriginWhitelist != null && shouldHandleURL(mOriginWhitelist, url)) {
+      //   return false;
+      // }
 
       launchIntent(view.getContext(), url);
       return true;
@@ -905,9 +905,9 @@ public class RNCWebViewManager extends SimpleViewManager<WebView> {
     public void setUrlPrefixesForDefaultIntent(ReadableArray specialUrls) {
       mUrlPrefixesForDefaultIntent = specialUrls;
     }
-    public void setOriginWhitelist(List<Pattern> originWhitelist) {
-      mOriginWhitelist = originWhitelist;
-    }
+    // public void setOriginWhitelist(List<Pattern> originWhitelist) {
+    //   mOriginWhitelist = originWhitelist;
+    // }
   }
 
   protected static class RNCWebChromeClient extends WebChromeClient implements LifecycleEventListener {
