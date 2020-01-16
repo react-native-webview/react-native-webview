70e312ad8aed8c0ceccf7aabbd2bf9615b37d77f
Tom Underhill <tomun@microsoft.com>
2020-01-15 17:50:52 -0800

    Slimmed down to one argument

--- react-native.config.js
+++ react-native.config.js
@@ -2,7 +2,7 @@
 
 if (process.argv.includes('--use-react-native-macos')) {
   process.argv = process.argv.filter(arg => arg !== '--use-react-native-macos');
-
+  process.argv.push('--config=metro.config.macos.js');
   module.exports = {
     reactNativePath: 'node_modules/react-native-macos',
   };
