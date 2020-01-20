45dee9cb67f3d53b0e32305501b259719f96bbda
Tom Underhill <tomun@microsoft.com>
2020-01-19 17:43:37 -0800

    index to example/index

--- example/android/app/src/main/java/com/example/MainApplication.java
+++ example/android/app/src/main/java/com/example/MainApplication.java
@@ -30,7 +30,7 @@ public class MainApplication extends Application implements ReactApplication {
 
         @Override
         protected String getJSMainModuleName() {
-          return "index";
+          return "example/index";
         }
       };
 
