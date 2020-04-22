e8effb1e0caca7e8e3f37059dd9eb55ce4e95506
shrinandhinijr <snandhini@torstar.ca>
2020-04-22 10:33:58 -0400

    Added the changes from main repo -uuid fix android

--- src/WebView.android.tsx
+++ src/WebView.android.tsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import 'react-native-get-random-values';
-import { v4 as uuid } from 'uuid';
 
 import {
   Image,
@@ -42,6 +40,10 @@ const RNCWebView = requireNativeComponent(
   'RNCWebView',
 ) as typeof NativeWebViewAndroid;
 const { resolveAssetSource } = Image;
+/**
+ * A simple counter to uniquely identify WebView instances. Do not use this for anything else.
+ */
+let uniqueRef = 0;
 
 /**
  * Renders a native WebView.
@@ -72,15 +74,13 @@ class WebView extends React.Component<AndroidWebViewProps, State> {
     lastErrorEvent: null,
   };
 
-  uniqueRef = uuid().replace(/-/g, '');
-
   webViewRef = React.createRef<NativeWebViewAndroid>();
 
-  componentDidMount = () => {
-    BatchedBridge.registerCallableModule(this.getMessagingModuleName(), this);
-  }
+  messagingModuleName = `WebViewMessageHandler${(uniqueRef += 1)}`;
 
-  getMessagingModuleName = () => `WebViewMessageHandler${this.uniqueRef}`;
+  componentDidMount = () => {
+    BatchedBridge.registerCallableModule(this.messagingModuleName, this);
+  };
 
   getCommands = () => UIManager.getViewManagerConfig('RNCWebView').Commands;
 
@@ -88,7 +88,7 @@ class WebView extends React.Component<AndroidWebViewProps, State> {
     UIManager.dispatchViewManagerCommand(
       this.getWebViewHandle(),
       this.getCommands().goForward,
-      undefined
+      undefined,
     );
   };
 
@@ -96,7 +96,7 @@ class WebView extends React.Component<AndroidWebViewProps, State> {
     UIManager.dispatchViewManagerCommand(
       this.getWebViewHandle(),
       this.getCommands().goBack,
-      undefined
+      undefined,
     );
   };
 
@@ -107,7 +107,7 @@ class WebView extends React.Component<AndroidWebViewProps, State> {
     UIManager.dispatchViewManagerCommand(
       this.getWebViewHandle(),
       this.getCommands().reload,
-      undefined
+      undefined,
     );
   };
 
@@ -115,7 +115,7 @@ class WebView extends React.Component<AndroidWebViewProps, State> {
     UIManager.dispatchViewManagerCommand(
       this.getWebViewHandle(),
       this.getCommands().stopLoading,
-      undefined
+      undefined,
     );
   };
 
@@ -123,7 +123,7 @@ class WebView extends React.Component<AndroidWebViewProps, State> {
     UIManager.dispatchViewManagerCommand(
       this.getWebViewHandle(),
       this.getCommands().requestFocus,
-      undefined
+      undefined,
     );
   };
 
@@ -137,25 +137,25 @@ class WebView extends React.Component<AndroidWebViewProps, State> {
 
   clearFormData = () => {
     UIManager.dispatchViewManagerCommand(
-       this.getWebViewHandle(),
-       this.getCommands().clearFormData,
-        undefined,
+      this.getWebViewHandle(),
+      this.getCommands().clearFormData,
+      undefined,
     );
-  }
+  };
 
   clearCache = (includeDiskFiles: boolean) => {
     UIManager.dispatchViewManagerCommand(
-       this.getWebViewHandle(),
-       this.getCommands().clearCache,
-       [includeDiskFiles],
+      this.getWebViewHandle(),
+      this.getCommands().clearCache,
+      [includeDiskFiles],
     );
   };
 
   clearHistory = () => {
     UIManager.dispatchViewManagerCommand(
-       this.getWebViewHandle(),
-       this.getCommands().clearHistory,
-        undefined,
+      this.getWebViewHandle(),
+      this.getCommands().clearHistory,
+      undefined,
     );
   };
 
@@ -194,7 +194,9 @@ class WebView extends React.Component<AndroidWebViewProps, State> {
 
   onLoadingStart = (event: WebViewNavigationEvent) => {
     const { onLoadStart } = this.props;
-    const { nativeEvent: { url } } = event;
+    const {
+      nativeEvent: { url },
+    } = event;
     this.startUrl = url;
     if (onLoadStart) {
       onLoadStart(event);
@@ -224,11 +226,13 @@ class WebView extends React.Component<AndroidWebViewProps, State> {
     if (onHttpError) {
       onHttpError(event);
     }
-  }
+  };
 
   onLoadingFinish = (event: WebViewNavigationEvent) => {
     const { onLoad, onLoadEnd } = this.props;
-    const { nativeEvent: { url } } = event;
+    const {
+      nativeEvent: { url },
+    } = event;
     if (onLoad) {
       onLoad(event);
     }
@@ -252,7 +256,9 @@ class WebView extends React.Component<AndroidWebViewProps, State> {
 
   onLoadingProgress = (event: WebViewProgressEvent) => {
     const { onLoadProgress } = this.props;
-    const { nativeEvent: { progress } } = event;
+    const {
+      nativeEvent: { progress },
+    } = event;
     if (progress === 1) {
       this.setState((state) => {
         if (state.viewState === 'LOADING') {
@@ -314,7 +320,7 @@ class WebView extends React.Component<AndroidWebViewProps, State> {
     const webViewStyles = [styles.container, styles.webView, style];
     const webViewContainerStyle = [styles.container, containerStyle];
 
-    if (typeof source !== "number" && source && 'method' in source) {
+    if (typeof source !== 'number' && source && 'method' in source) {
       if (source.method === 'POST' && source.headers) {
         console.warn(
           'WebView: `source.headers` is not supported when using POST.',
@@ -324,8 +330,8 @@ class WebView extends React.Component<AndroidWebViewProps, State> {
       }
     }
 
-    const NativeWebView
-      = (nativeConfig.component as typeof NativeWebViewAndroid) || RNCWebView;
+    const NativeWebView =
+      (nativeConfig.component as typeof NativeWebViewAndroid) || RNCWebView;
 
     const onShouldStartLoadWithRequest = createOnShouldStartLoadWithRequest(
       this.onShouldStartLoadWithRequestCallback,
@@ -339,7 +345,7 @@ class WebView extends React.Component<AndroidWebViewProps, State> {
         key="webViewKey"
         {...otherProps}
         messagingEnabled={typeof onMessage === 'function'}
-        messagingModuleName={this.getMessagingModuleName()}
+        messagingModuleName={this.messagingModuleName}
         onLoadingError={this.onLoadingError}
         onLoadingFinish={this.onLoadingFinish}
         onLoadingProgress={this.onLoadingProgress}
