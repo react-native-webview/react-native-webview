f2f6fafd5e68f923a9a44752fb6010d8a3cc18db
Tero Paananen <tero.paananen@showell.com>
2020-09-04 11:41:23 +0300

    Update bridge implementation

--- windows/WebViewBridgeComponent/WebBridge.cpp
+++ windows/WebViewBridgeComponent/WebBridge.cpp
@@ -1,22 +1,19 @@
-﻿#include "pch.h"
+#include "pch.h"
 #include "WebBridge.h"
 #include "WebBridge.g.cpp"
 
 namespace winrt::WebViewBridgeComponent::implementation
 {
-  winrt::event_token WebBridge::MessagePostEvent(Windows::Foundation::EventHandler<winrt::hstring> const& handler)
-  {
-    return m_messageEvent.add(handler);
-  }
-
-  void WebBridge::MessagePostEvent(winrt::event_token const& token) noexcept
-  {
-    m_messageEvent.remove(token);
-  }
-
-  void WebBridge::PostMessage(winrt::hstring message)
-  {
-    m_messageEvent(*this, message);
-  }
-
+    void WebBridge::PostMessage(hstring const& message)
+    {
+        m_messageEvent(*this, message);
+    }
+    winrt::event_token WebBridge::MessagePostEvent(Windows::Foundation::EventHandler<hstring> const& handler)
+    {
+        return m_messageEvent.add(handler);
+    }
+    void WebBridge::MessagePostEvent(winrt::event_token const& token) noexcept
+    {
+        m_messageEvent.remove(token);
+    }
 }
--- windows/WebViewBridgeComponent/WebBridge.h
+++ windows/WebViewBridgeComponent/WebBridge.h
@@ -1,23 +1,21 @@
-﻿#pragma once
-
+#pragma once
 #include "WebBridge.g.h"
+
 
 namespace winrt::WebViewBridgeComponent::implementation
 {
     struct WebBridge : WebBridgeT<WebBridge>
     {
-    public:  
-      WebBridge() = default;
-      void PostMessage(winrt::hstring message);
-      winrt::event_token MessagePostEvent(Windows::Foundation::EventHandler<winrt::hstring> const& handler);
-      void MessagePostEvent(winrt::event_token const& token) noexcept;
+        WebBridge() = default;
+
+        void PostMessage(hstring const& message);
+        winrt::event_token MessagePostEvent(Windows::Foundation::EventHandler<hstring> const& handler);
+        void MessagePostEvent(winrt::event_token const& token) noexcept;
+
     private:
       winrt::event<Windows::Foundation::EventHandler<winrt::hstring>> m_messageEvent;
     };
-
-    
 }
-
 namespace winrt::WebViewBridgeComponent::factory_implementation
 {
     struct WebBridge : WebBridgeT<WebBridge, implementation::WebBridge>
