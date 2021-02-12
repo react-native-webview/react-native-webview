1fee425ddd354938796c09f8b147923c7e244649
niwani <niwani@microsoft.com>
2021-02-12 20:37:17 +0530

    Missig brace added

--- apple/RNCWebView.m
+++ apple/RNCWebView.m
@@ -536,7 +536,6 @@ static NSDictionary* customCertificatesForHost;
 {
     // Check for a static html source first
     NSString *html = [RCTConvert NSString:_source[@"html"]];
-    NSString *headerCookie = [RCTConvert NSString:_source[@"headers"][@"cookie"]]; 
     if (html) {
         NSURL *baseURL = [RCTConvert NSURL:_source[@"baseUrl"]];
         if (!baseURL) {
@@ -544,12 +543,16 @@ static NSDictionary* customCertificatesForHost;
         }
         [_webView loadHTMLString:html baseURL:baseURL];
         return;
-    } else if(headerCookie) {
-        NSDictionary *headers = [NSDictionary dictionaryWithObjectsAndKeys:headerCookie,@"Set-Cookie",nil];
-        NSURL *urlString = [NSURL URLWithString:_source[@"uri"]];
-        NSArray *httpCookies = [NSHTTPCookie cookiesWithResponseHeaderFields:headers forURL:urlString ];
-        for (NSHTTPCookie *httpCookie in httpCookies) {
-            [_webView.configuration.websiteDataStore.httpCookieStore setCookie:httpCookie completionHandler:nil];
+    } 
+    //Add cookie for subsequent resource requests sent by page itself, if cookie was set in headers on WebView
+    NSString *headerCookie = [RCTConvert NSString:_source[@"headers"][@"cookie"]]; 
+    if(headerCookie) {
+      NSDictionary *headers = [NSDictionary dictionaryWithObjectsAndKeys:headerCookie,@"Set-Cookie",nil];
+      NSURL *urlString = [NSURL URLWithString:_source[@"uri"]];
+      NSArray *httpCookies = [NSHTTPCookie cookiesWithResponseHeaderFields:headers forURL:urlString];
+      for (NSHTTPCookie *httpCookie in httpCookies) {
+          [_webView.configuration.websiteDataStore.httpCookieStore setCookie:httpCookie completionHandler:nil];
+      }
     }
 
     NSURLRequest *request = [self requestForSource:_source];
