9aa85f1258a229ce307aa7bf3033b44bffcbfe26
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2021-07-13 15:00:30 +0200

    chore: migrate to react-native-test-app
    - Android test app now requires JDK 11
    - Bump react-native* to 0.64
    - Bump jest to 26.6.3
    - Bump typescript to 4.5.5

--- example/metro.config.js
+++ example/metro.config.js
@@ -5,7 +5,34 @@
  * @format
  */
 
+const path = require("path");
+
+const exclusionList = (() => {
+  try {
+    return require("metro-config/src/defaults/exclusionList");
+  } catch (_) {
+    // `blacklist` was renamed to `exclusionList` in 0.60
+    return require("metro-config/src/defaults/blacklist");
+  }
+})();
+
+const blockList = exclusionList([
+  /node_modules\/.*\/node_modules\/react-native\/.*/,
+
+  // This stops "react-native run-windows" from causing the metro server to
+  // crash if its already running
+  new RegExp(`${path.join(__dirname, "windows").replace(/[/\\]+/g, "/")}.*`),
+
+  // Workaround for `EBUSY: resource busy or locked, open '~\msbuild.ProjectImports.zip'`
+  // when building with `yarn windows --release`
+  /.*\.ProjectImports\.zip/,
+]);
+
 module.exports = {
+  resolver: {
+    blacklistRE: blockList,
+    blockList,
+  },
   transformer: {
     getTransformOptions: async () => ({
       transform: {
