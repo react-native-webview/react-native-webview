97501a3b9f3810103835845f7fdbed51031e4e56
Arjan Zuidema <arjan@myalbum.com>
2022-01-10 08:18:03 +0100

    Added textInteractionEnabled prop to WebViewTypes

--- src/WebViewTypes.ts
+++ src/WebViewTypes.ts
@@ -369,6 +369,7 @@ export interface IOSNativeWebViewProps extends CommonNativeWebViewProps {
   injectedJavaScriptBeforeContentLoadedForMainFrameOnly?: boolean;
   onFileDownload?: (event: FileDownloadEvent) => void;
   limitsNavigationsToAppBoundDomains?: boolean;
+  textInteractionEnabled?: boolean;
   mediaCapturePermissionGrantType?: MediaCapturePermissionGrantType;
 }
 
@@ -670,6 +671,15 @@ export interface IOSWebViewProps extends WebViewSharedProps {
    * @platform ios
    */
   limitsNavigationsToAppBoundDomains?: boolean;
+
+  /**
+   * If false indicates to WebKit that a WKWebView will not interact with text, thus
+   * not showing a text selection loop. Only applicable for iOS 15 or greater.
+   * 
+   * Defaults to true.
+   * @platform ios
+   */
+  textInteractionEnabled?: boolean;
 
   /**
    * This property specifies how to handle media capture permission requests.
