8f758f8614b7252ce156c1668aeac6138e74225b
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2022-02-17 13:59:44 +0100

    chore: migrate to react-native-test-app (#2148)
    * chore: migrate to react-native-test-app
    
    - Android test app now requires JDK 11
    - Bump react-native* to 0.64
    - Bump jest to 26.6.3
    - Bump typescript to 4.5.5
    
    * unbreak Windows build
    
    * fix Windows autolink
    
    * don't use nuget and default to x64
    
    * add logging during deploy
    
    * restore certificate for deployment
    
    * update app id
    
    * reuse appxmanifest

--- react-native.config.js
+++ react-native.config.js
@@ -1,63 +1,81 @@
-/**
- * This cli config is needed for the coexistance of react-native and other
- * out-of-tree implementations such react-native-macos.
- * The following issue is tracked by
- * https://github.com/react-native-community/discussions-and-proposals/issues/182
- *
- * The work-around involves having a metro.config.js for each out-of-tree
- * platform, i.e. metro.config.js for react-native and 
- * metro.config.macos.js for react-native-macos.
- * This react-native.config.js looks for a --use-react-native-macos
- * switch and when present pushes --config=metro.config.macos.js 
- * and specifies reactNativePath: 'node_modules/react-native-macos'.
- * The metro.config.js has to blacklist 'node_modules/react-native-macos', 
- * and conversely metro.config.macos.js has to blacklist 'node_modules/react-native'.
- */
-'use strict';
-
+const fs = require('fs');
 const path = require('path');
 
-const macSwitch = '--use-react-native-macos';
-const windowsSwitch = '--use-react-native-windows';
+const windowsProjectFile = path.join(
+  'node_modules',
+  '.generated',
+  'windows',
+  'ReactTestApp',
+  'ReactTestApp.vcxproj',
+);
 
-if (process.argv.includes(macSwitch)) {
-  process.argv = process.argv.filter(arg => arg !== macSwitch);
-  process.argv.push('--config=metro.config.macos.js');
-  module.exports = {
-    reactNativePath: 'node_modules/react-native-macos',
-  };
-}
-else if (process.argv.includes(windowsSwitch)) {
-  process.argv = process.argv.filter(arg => arg !== windowsSwitch);
-  process.argv.push('--config=metro.config.windows.js');
-  module.exports = {
-    reactNativePath: 'node_modules/react-native-windows',
-  };
-}
-else {
-  module.exports = {
-    project: {
-      ios: {
-        project: 'example/ios/',
-      },
-      android: {
-        sourceDir: 'example/android',
+module.exports = {
+  dependencies: {
+    // Help rn-cli find and autolink this library
+    'react-native-webview': {
+      root: __dirname,
+    },
+  },
+  dependency: {
+    platforms: {
+      windows: {
+        sourceDir: 'windows',
+        solutionFile: 'ReactNativeWebView.sln',
+        projects: [
+          {
+            projectFile: 'ReactNativeWebView/ReactNativeWebView.vcxproj',
+            directDependency: true,
+          },
+        ],
       },
     },
-  };
-}
+  },
+  project: {
+    android: {
+      sourceDir: path.join('example', 'android'),
+      manifestPath: path.relative(
+        path.join(__dirname, 'example', 'android'),
+        path.join(
+          path.dirname(require.resolve('react-native-test-app/package.json')),
+          'android',
+          'app',
+          'src',
+          'main',
+          'AndroidManifest.xml',
+        ),
+      ),
+    },
+    ios: {
+      project: (() => {
+        const {
+          packageSatisfiesVersionRange,
+        } = require('react-native-test-app/scripts/configure');
+        if (
+          packageSatisfiesVersionRange(
+            '@react-native-community/cli-platform-ios',
+            '<5.0.2',
+          )
+        ) {
+          // Prior to @react-native-community/cli-platform-ios v5.0.0,
+          // `project` was only used to infer `sourceDir` and `podfile`.
+          return 'example/ios/ReactTestApp-Dummy.xcodeproj';
+        }
 
-module.exports.dependency = {
-  platforms: {
-    windows: {
-      sourceDir: 'windows',
-      solutionFile: 'ReactNativeWebView.sln',
-      projects: [
-        {
-          projectFile: 'ReactNativeWebView/ReactNativeWebView.vcxproj',
-          directDependency: true,
-        },
-      ],
+        // `sourceDir` and `podfile` detection was fixed in
+        // @react-native-community/cli-platform-ios v5.0.2 (see
+        // https://github.com/react-native-community/cli/pull/1444).
+        return 'node_modules/.generated/ios/ReactTestApp.xcodeproj';
+      })(),
+    },
+    windows: fs.existsSync(windowsProjectFile) && {
+      sourceDir: path.join('example', 'windows'),
+      solutionFile: 'WebviewExample.sln',
+      project: {
+        projectFile: path.relative(
+          path.join(__dirname, 'example', 'windows'),
+          windowsProjectFile,
+        ),
+      },
     },
   },
 };
