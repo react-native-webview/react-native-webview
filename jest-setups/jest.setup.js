04ff13f3f38a5289b10591d76a48f786e2f2fbad
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2022-02-17 04:18:02 -0800

    reuse appxmanifest

--- jest-setups/jest.setup.js
+++ jest-setups/jest.setup.js
@@ -4,7 +4,7 @@ const { platform } = require('./jest.setup.windows');
 
 switch (platform) {
     case "windows":
-        const webViewWindowsAppId = '40411fc5-8e92-4d46-b68d-b62df44b1366_7c3z4tcdk8r62!App';
+        const webViewWindowsAppId = 'WebViewWindows_3x6rhkkr9xcf6!App';
         module.exports = {
             capabilities: windowsAppDriverCapabilities(webViewWindowsAppId)
         }
