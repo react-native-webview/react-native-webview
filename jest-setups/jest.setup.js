1829028d238ada1150d5009fcd77a5e22d2fd62f
Chiara Mooney <34109996+chiaramooney@users.noreply.github.com>
2022-03-02 15:42:59 -0800

    Fix Appium

--- jest-setups/jest.setup.js
+++ jest-setups/jest.setup.js
@@ -4,7 +4,7 @@ const { platform } = require('./jest.setup.windows');
 
 switch (platform) {
     case "windows":
-        const webViewWindowsAppId = 'ReactTestApp!App';
+        const webViewWindowsAppId = '40411fc5-8e92-4d46-b68d-b62df44b1366_3x6rhkkr9xcf6!App';
         module.exports = {
             capabilities: windowsAppDriverCapabilities(webViewWindowsAppId)
         }
