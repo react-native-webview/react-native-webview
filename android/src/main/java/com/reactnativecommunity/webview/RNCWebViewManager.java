fd4f5b5a59c724be5df890b311fe300397d4daaf
ccreonopoulos <creon.creonopoulos@us.isobar.com>
2022-10-19 13:56:17 -0500

    Cookie header key passed from react native case insensitive check to add to android webview

--- android/src/main/java/com/reactnativecommunity/webview/RNCWebViewManager.java
+++ android/src/main/java/com/reactnativecommunity/webview/RNCWebViewManager.java
@@ -565,6 +565,10 @@ public class RNCWebViewManager extends SimpleViewManager<WebView> {
                 view.getSettings().setUserAgentString(headers.getString(key));
               }
             } else {
+              if (key.equalsIgnoreCase("Cookie")) {
+                CookieManager.getInstance().setCookie(url, headers.getString(key));
+              }
+              
               headerMap.put(key, headers.getString(key));
             }
           }
