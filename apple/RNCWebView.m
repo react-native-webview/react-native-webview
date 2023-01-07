ae393b5caf84f089da17d25b7e82a62961a0a13d
Donald <mail.donaldm@gmail.com>
2023-01-07 00:10:50 -0800

    add logs

--- apple/RNCWebView.m
+++ apple/RNCWebView.m
@@ -127,17 +127,17 @@ static WKContentRuleList * _contentRuleList;
     NSConditionLock *lock = [[NSConditionLock alloc] initWithCondition:0];
     [[WKContentRuleListStore defaultStore] lookUpContentRuleListForIdentifier: contentRuleId completionHandler:^(WKContentRuleList *contentRuleList, NSError *error) {
       if (error != nil) {
-        // NSLog(@"DONDEBUG - Can not find content rule list %@ from store: %@",  contentRuleId, error.localizedDescription);
-        // NSLog(@"DONDEBUG - Can not find content rule list %@ from store: %@",  contentRuleId, error.localizedRecoverySuggestion);
-        // NSLog(@"DONDEBUG - Can not find content rule list %@ from store: %@",  contentRuleId, error.localizedFailureReason);
+        NSLog(@"DONDEBUG - Can not find content rule list %@ from store: %@",  contentRuleId, error.localizedDescription);
+        NSLog(@"DONDEBUG - Can not find content rule list %@ from store: %@",  contentRuleId, error.localizedRecoverySuggestion);
+        NSLog(@"DONDEBUG - Can not find content rule list %@ from store: %@",  contentRuleId, error.localizedFailureReason);
         NSURL *podBundleURL = [[[NSBundle mainBundle] resourceURL] URLByAppendingPathComponent:podBundleName];
         NSBundle *podBundle = [NSBundle bundleWithURL:podBundleURL];
         NSString *path = [podBundle pathForResource:contentRuleFile ofType:@"json"];
         NSString *content = [NSString stringWithContentsOfFile:path encoding:NSUTF8StringEncoding error:nil];
-        // NSLog(@"DONDEBUG - PATH FOUND from podBundleURL: %@", podBundleURL);
-        // NSLog(@"DONDEBUG - PATH FOUND from podBundle: %@", podBundle);
-        // NSLog(@"DONDEBUG - PATH FOUND from path: %@", path);
-        // NSLog(@"DONDEBUG - CONTENT FOUND from content: %@", content);
+        NSLog(@"DONDEBUG - PATH FOUND from podBundleURL: %@", podBundleURL);
+        NSLog(@"DONDEBUG - PATH FOUND from podBundle: %@", podBundle);
+        NSLog(@"DONDEBUG - PATH FOUND from path: %@", path);
+        NSLog(@"DONDEBUG - CONTENT FOUND from content: %@", content);
 
         [[WKContentRuleListStore defaultStore] compileContentRuleListForIdentifier: contentRuleId encodedContentRuleList:content completionHandler:^(WKContentRuleList *contentRuleList, NSError *error) {
           if (error != nil) {
@@ -155,9 +155,9 @@ static WKContentRuleList * _contentRuleList;
         [lock unlockWithCondition:1];
       }
     }];
-    // NSLog(@"DONDEBUG - OUTSIDE IT ALL");
+    NSLog(@"DONDEBUG - OUTSIDE IT ALL");
     // Wait max 200ms for content rule to be loaded.
-    [lock lockWhenCondition:1 beforeDate:[NSDate dateWithTimeIntervalSinceNow:2]];
+    [lock lockWhenCondition:1 beforeDate:[NSDate dateWithTimeIntervalSinceNow:10]];
   }
 }
 #endif
