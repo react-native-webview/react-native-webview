e17a79b9aa1ea41c9e52ae67fb6afd91cfa144f2
Thibault Malbranche <malbranche.thibault@gmail.com>
2023-06-10 13:09:46 +0200

    fix: Revert "Missing android.support.FILE_PROVIDER_PATHS meta-data on some OEMs (#2952)"
    This reverts commit 75e7801a6255b37f6887d29d10b9ba688e051416.

--- android/src/main/java/com/reactnativecommunity/webview/RNCWebViewFileProvider.java
+++ android/src/main/java/com/reactnativecommunity/webview/RNCWebViewFileProvider.java
@@ -1,7 +1,6 @@
 package com.reactnativecommunity.webview;
 
 import androidx.core.content.FileProvider;
-import com.reactnativecommunity.webview.R;
 
 /**
  * Providing a custom {@code FileProvider} prevents manifest {@code <provider>} name collisions.
@@ -9,7 +8,7 @@ import com.reactnativecommunity.webview.R;
  * See https://developer.android.com/guide/topics/manifest/provider-element.html for details.
  */
 public class RNCWebViewFileProvider extends FileProvider {
-  public RNCWebViewFileProvider() {
-    super(R.xml.file_provider_paths);
-  }
+
+  // This class intentionally left blank.
+
 }
