3424a39349fa55d4333a5bec50b2dc7388ee41ab
camou <3.ria.eboda@gmail.com>
2023-11-21 12:41:31 +0900

    feat: add callback

--- android/src/newarch/com/reactnativecommunity/webview/RNCWebViewManager.java
+++ android/src/newarch/com/reactnativecommunity/webview/RNCWebViewManager.java
@@ -16,6 +16,7 @@ import com.facebook.react.viewmanagers.RNCWebViewManagerInterface;
 import com.facebook.react.views.scroll.ScrollEventType;
 import com.reactnativecommunity.webview.events.TopCustomMenuSelectionEvent;
 import com.reactnativecommunity.webview.events.TopHttpErrorEvent;
+import com.reactnativecommunity.webview.events.TopDownloadEvent;
 import com.reactnativecommunity.webview.events.TopLoadingErrorEvent;
 import com.reactnativecommunity.webview.events.TopLoadingFinishEvent;
 import com.reactnativecommunity.webview.events.TopLoadingProgressEvent;
@@ -521,6 +522,7 @@ public class RNCWebViewManager extends ViewGroupManager<RNCWebViewWrapper>
         export.put(TopRenderProcessGoneEvent.EVENT_NAME, MapBuilder.of("registrationName", "onRenderProcessGone"));
         export.put(TopCustomMenuSelectionEvent.EVENT_NAME, MapBuilder.of("registrationName", "onCustomMenuSelection"));
         export.put(TopOpenWindowEvent.EVENT_NAME, MapBuilder.of("registrationName", "onOpenWindow"));
+        export.put(TopDownloadEvent.EVENT_NAME, MapBuilder.of("registrationName", "onDownloadBase64"));
         return export;
     }
 
