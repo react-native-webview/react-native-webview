90031d70ee730f860540753ae76e9518e26cb0e2
João Loureiro <1649425+jpcloureiro@users.noreply.github.com>
2024-01-11 16:22:54 +0000

    Add the ability for the Android application to process deep links within webviews.
    Add allow list to reroute links to deeplink functionality in the RNWebViewManager
    
    code ported from https://github.com/MetaMask/metamask-mobile/pull/3348

--- android/src/main/java/com/reactnativecommunity/webview/RNCWebViewClient.java
+++ android/src/main/java/com/reactnativecommunity/webview/RNCWebViewClient.java
@@ -1,7 +1,9 @@
 package com.reactnativecommunity.webview;
 
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.net.http.SslError;
 import android.os.Build;
 import android.os.SystemClock;
@@ -32,6 +34,7 @@ import com.reactnativecommunity.webview.events.TopLoadingStartEvent;
 import com.reactnativecommunity.webview.events.TopRenderProcessGoneEvent;
 import com.reactnativecommunity.webview.events.TopShouldStartLoadWithRequestEvent;
 
+import java.util.Arrays;
 import java.util.concurrent.atomic.AtomicReference;
 
 public class RNCWebViewClient extends WebViewClient {
@@ -41,6 +44,8 @@ public class RNCWebViewClient extends WebViewClient {
     protected boolean mLastLoadFailed = false;
     protected RNCWebView.ProgressChangedFilter progressChangedFilter = null;
     protected @Nullable String ignoreErrFailedForThisURL = null;
+    // Allow list is used to support specific deeplinks in the webview.
+    private final String[] DEEPLINK_ALLOW_LIST = new String[] {"https://twitter.com/i/wallet/verify"};
     protected @Nullable RNCBasicAuthCredential basicAuthCredential = null;
 
     public void setIgnoreErrFailedForThisURL(@Nullable String url) {
@@ -90,37 +95,48 @@ public class RNCWebViewClient extends WebViewClient {
         final boolean isJsDebugging = ((ReactContext) view.getContext()).getJavaScriptContextHolder().get() == 0;
 
         if (!isJsDebugging && rncWebView.mCatalystInstance != null) {
-            final Pair<Double, AtomicReference<RNCWebViewModuleImpl.ShouldOverrideUrlLoadingLock.ShouldOverrideCallbackState>> lock = RNCWebViewModuleImpl.shouldOverrideUrlLoadingLock.getNewLock();
-            final double lockIdentifier = lock.first;
-            final AtomicReference<RNCWebViewModuleImpl.ShouldOverrideUrlLoadingLock.ShouldOverrideCallbackState> lockObject = lock.second;
+          final Pair<Double, AtomicReference<RNCWebViewModuleImpl.ShouldOverrideUrlLoadingLock.ShouldOverrideCallbackState>> lock = RNCWebViewModuleImpl.shouldOverrideUrlLoadingLock.getNewLock();
+          final double lockIdentifier = lock.first;
+          final AtomicReference<RNCWebViewModuleImpl.ShouldOverrideUrlLoadingLock.ShouldOverrideCallbackState> lockObject = lock.second;
 
-            final WritableMap event = createWebViewEvent(view, url);
-            event.putDouble("lockIdentifier", lockIdentifier);
-            rncWebView.sendDirectMessage("onShouldStartLoadWithRequest", event);
+          final WritableMap event = createWebViewEvent(view, url);
+          event.putDouble("lockIdentifier", lockIdentifier);
+          rncWebView.sendDirectMessage("onShouldStartLoadWithRequest", event);
 
-            try {
-                assert lockObject != null;
-                synchronized (lockObject) {
-                    final long startTime = SystemClock.elapsedRealtime();
-                    while (lockObject.get() == RNCWebViewModuleImpl.ShouldOverrideUrlLoadingLock.ShouldOverrideCallbackState.UNDECIDED) {
-                        if (SystemClock.elapsedRealtime() - startTime > SHOULD_OVERRIDE_URL_LOADING_TIMEOUT) {
-                            FLog.w(TAG, "Did not receive response to shouldOverrideUrlLoading in time, defaulting to allow loading.");
-                            RNCWebViewModuleImpl.shouldOverrideUrlLoadingLock.removeLock(lockIdentifier);
-                            return false;
-                        }
-                        lockObject.wait(SHOULD_OVERRIDE_URL_LOADING_TIMEOUT);
-                    }
+          try {
+            assert lockObject != null;
+            synchronized (lockObject) {
+              final long startTime = SystemClock.elapsedRealtime();
+              while (lockObject.get() == RNCWebViewModuleImpl.ShouldOverrideUrlLoadingLock.ShouldOverrideCallbackState.UNDECIDED) {
+                if (SystemClock.elapsedRealtime() - startTime > SHOULD_OVERRIDE_URL_LOADING_TIMEOUT) {
+                  FLog.w(TAG, "Did not receive response to shouldOverrideUrlLoading in time, defaulting to allow loading.");
+                  RNCWebViewModuleImpl.shouldOverrideUrlLoadingLock.removeLock(lockIdentifier);
+                  return false;
                 }
-            } catch (InterruptedException e) {
-                FLog.e(TAG, "shouldOverrideUrlLoading was interrupted while waiting for result.", e);
-                RNCWebViewModuleImpl.shouldOverrideUrlLoadingLock.removeLock(lockIdentifier);
-                return false;
+                lockObject.wait(SHOULD_OVERRIDE_URL_LOADING_TIMEOUT);
+              }
             }
-
-            final boolean shouldOverride = lockObject.get() == RNCWebViewModuleImpl.ShouldOverrideUrlLoadingLock.ShouldOverrideCallbackState.SHOULD_OVERRIDE;
+          } catch (InterruptedException e) {
+            FLog.e(TAG, "shouldOverrideUrlLoading was interrupted while waiting for result.", e);
             RNCWebViewModuleImpl.shouldOverrideUrlLoadingLock.removeLock(lockIdentifier);
+            return false;
+          }
 
-            return shouldOverride;
+          final boolean shouldOverride = lockObject.get() == RNCWebViewModuleImpl.ShouldOverrideUrlLoadingLock.ShouldOverrideCallbackState.SHOULD_OVERRIDE;
+          RNCWebViewModuleImpl.shouldOverrideUrlLoadingLock.removeLock(lockIdentifier);
+
+          return shouldOverride;
+        } else if (url != null && Arrays.asList(DEEPLINK_ALLOW_LIST).contains(url)) {
+          // This case is used to support deeplinking within the webview. We are limiting this but
+          // if more links are to be supported we should consider a more scaleable solution. That is
+          // secure and scaleable.
+          Intent intent = new Intent(Intent.ACTION_VIEW);
+          intent.setData(Uri.parse(url));
+          if(intent.resolveActivity(view.getContext().getPackageManager()) != null) {
+            view.getContext().startActivity(intent);
+            return true;
+          } else
+            return false;
         } else {
             FLog.w(TAG, "Couldn't use blocking synchronous call for onShouldStartLoadWithRequest due to debugging or missing Catalyst instance, falling back to old event-and-load.");
             progressChangedFilter.setWaitingForCommandLoadUrl(true);
