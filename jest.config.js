3eb010ca922e42bb842b82d077cb22674f98d6d8
Thibault Malbranche <malbranche.thibault@gmail.com>
2024-03-04 14:12:47 +0100

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,6 @@
 module.exports = {
+  // Automatically clear mock calls and instances between every test
+  clearMocks: true,
   preset: 'react-native',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
 };
