2e6888c5380f9b2c6b89023df37bc721a99dac83
Saad Najmi <sanajmi@microsoft.com>
2024-05-13 16:04:34 -0700

    fix(iOS): Implement `[RCTConvert WKDataDetectorTypes:]`

---
+++ apple/RCTConvert+WKDataDetectorTypes.h
@@ -0,0 +1,9 @@
+ #import <WebKit/WebKit.h>
+
+ #import <React/RCTConvert.h>
+
+@interface RCTConvert (WKDataDetectorTypes)
+
++ (WKDataDetectorTypes)WKDataDetectorTypes:(id)json;
+
+@end
---
+++ apple/RCTConvert+WKDataDetectorTypes.m
@@ -0,0 +1,19 @@
+#import <WebKit/WebKit.h>
+
+#import "RCTConvert.h"
+
+RCT_MULTI_ENUM_CONVERTER(
+     WKDataDetectorTypes,
+     (@{
+       @"none" : @(WKDataDetectorTypeNone),
+       @"phoneNumber" : @(WKDataDetectorTypePhoneNumber),
+       @"link" : @(WKDataDetectorTypeLink),
+       @"address" : @(WKDataDetectorTypeAddress),
+       @"calendarEvent" : @(WKDataDetectorTypeCalendarEvent),
+       @"trackingNumber" : @(WKDataDetectorTypeTrackingNumber),
+       @"flightNumber" : @(WKDataDetectorTypeFlightNumber),
+       @"lookupSuggestion" : @(WKDataDetectorTypeLookupSuggestion),
+       @"all" : @(WKDataDetectorTypeAll),
+     }),
+     WKDataDetectorTypeNone,
+     unsignedLongLongValue)
