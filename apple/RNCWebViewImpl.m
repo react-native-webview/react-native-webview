1b478056a7988990c5bad5ef108edbb19964d5df
Jean-Christophe Cazeaux <jccazeaux79@gmail.com>
2024-08-20 16:32:51 +0200

    fix: bug where setCookie completionHandler is never called (#3470)
    * Fix bug where setCookie completionHandler is never called on first syncCookiesToWebView
    
    * Get configuration in webview

--- apple/RNCWebViewImpl.m
+++ apple/RNCWebViewImpl.m
@@ -837,6 +837,9 @@ RCTAutoInsetsProtocol>
   NSString *allowingReadAccessToURL = _allowingReadAccessToURL;
 
   [self syncCookiesToWebView:^{
+    // Add observer to sync cookies from webview to sharedHTTPCookieStorage
+    [webView.configuration.websiteDataStore.httpCookieStore addObserver:self];
+    
     // Because of the way React works, as pages redirect, we actually end up
     // passing the redirect urls back here, so we ignore them if trying to load
     // the same url. We'll expose a call to 'reload' to allow a user to load
@@ -1830,9 +1833,7 @@ didFinishNavigation:(WKNavigation *)navigation
       if(!_incognito && !_cacheEnabled) {
         wkWebViewConfig.websiteDataStore = [WKWebsiteDataStore nonPersistentDataStore];
       }
-      [self syncCookiesToWebView:^{
-        [wkWebViewConfig.websiteDataStore.httpCookieStore addObserver:self];
-      }];
+      [self syncCookiesToWebView:^{}];
     } else {
       NSMutableString *script = [NSMutableString string];
 
