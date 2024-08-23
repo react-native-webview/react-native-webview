a0bcbcc86ef7af85ea2674344a5e560cece65378
Hassan Yousefi <hexboy2011@gmail.com>
2024-08-23 11:58:09 +0330

    fix: Implement missing methods of RNCWebViewManagerInterface in RNCWebViewManager

--- android/src/newarch/com/reactnativecommunity/webview/RNCWebViewManager.java
+++ android/src/newarch/com/reactnativecommunity/webview/RNCWebViewManager.java
@@ -342,6 +342,9 @@ public class RNCWebViewManager extends ViewGroupManager<RNCWebViewWrapper>
     public void setAllowsInlineMediaPlayback(RNCWebViewWrapper view, boolean value) {}
 
     @Override
+    public void setAllowsPictureInPictureMediaPlayback(RNCWebViewWrapper view, boolean value) {}
+
+    @Override
     public void setAllowsAirPlayForMediaPlayback(RNCWebViewWrapper view, boolean value) {}
 
     @Override
@@ -388,6 +391,9 @@ public class RNCWebViewManager extends ViewGroupManager<RNCWebViewWrapper>
 
     @Override
     public void setPullToRefreshEnabled(RNCWebViewWrapper view, boolean value) {}
+
+    @Override
+    public void setRefreshControlLightMode(RNCWebViewWrapper view, boolean value) {}
 
     @Override
     public void setScrollEnabled(RNCWebViewWrapper view, boolean value) {}
