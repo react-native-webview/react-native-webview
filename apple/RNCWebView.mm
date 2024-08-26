7e49457d62234d475025fca5825502846c8eabbf
Wojciech Lewicki <wojciech.lewicki@swmansion.com>
2024-08-26 10:25:17 +0200

    feat: add load method (#3537)

--- apple/RNCWebView.mm
+++ apple/RNCWebView.mm
@@ -60,6 +60,12 @@ auto stringToOnLoadingFinishNavigationTypeEnum(std::string value) {
 }
 #endif // !TARGET_OS_OSX
 
+// Needed because of this: https://github.com/facebook/react-native/pull/37274
++ (void)load
+{
+  [super load];
+}
+
 - (instancetype)initWithFrame:(CGRect)frame
 {
     if (self = [super initWithFrame:frame]) {
