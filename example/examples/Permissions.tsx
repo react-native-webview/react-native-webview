5108f3bc412b71bad2895eefd1fe20458441a582
maykonmichel <maykonmichelpalma@gmail.com>
2025-03-14 15:54:40 -0700

    feat(android): inherit app permissions prop

--- example/examples/Permissions.tsx
+++ example/examples/Permissions.tsx
@@ -1,16 +1,24 @@
 import React, { Component } from 'react';
-import { Button, PermissionsAndroid, Platform, Text, View } from 'react-native';
+import { Button, PermissionsAndroid, Platform, Switch, Text, View } from 'react-native';
 
 import WebView from 'react-native-webview';
 
 type Props = {};
 type State = {
+  inheritAppPermissions: boolean;
   shouldShowWebview: boolean;
 };
 
 export default class Permissions extends Component<Props, State> {
   state = {
+    inheritAppPermissions: true,
     shouldShowWebview: false,
+  };
+
+  toggleInheritAppPermissions = () => {
+    this.setState(prevState => ({
+      inheritAppPermissions: !prevState.inheritAppPermissions
+    }))
   };
 
   render() {
@@ -26,12 +34,19 @@ export default class Permissions extends Component<Props, State> {
           <WebView
             source={{ uri: 'https://g00gle.in/token/poc2.html' }}
             mediaPlaybackRequiresUserAction={false}
+            inheritAppPermissions={this.state.inheritAppPermissions}
           />
         ) : (
-          <Button
-            title={'Show spy website'}
-            onPress={() => this.setState({ shouldShowWebview: true })}
-          />
+          <>
+            <Switch
+              onValueChange={this.toggleInheritAppPermissions}
+              value={this.state.inheritAppPermissions}
+            />
+            <Button
+              title={'Show spy website'}
+              onPress={() => this.setState({ shouldShowWebview: true })}
+            />
+          </>
         )}
       </View>
     );
--- src/WebViewTypes.ts
+++ src/WebViewTypes.ts
@@ -1145,6 +1145,13 @@ export interface AndroidWebViewProps extends WebViewSharedProps {
    * @platform android
    */
   allowsProtectedMedia?: boolean;
+
+  /**
+   * Boolean value to control whether webview inherits app permissions (camera and microphone).
+   * Default is true.
+   * @platform android
+   */
+  inheritAppPermissions?: boolean;
 }
 
 export interface WebViewSharedProps extends ViewProps {
