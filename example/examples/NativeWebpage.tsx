11954ca8ea71b8d8ae31c8bdaae78e146ac77ea8
Richard Kunkli <richard@kunkli.com>
2025-10-20 10:19:01 +0200

    update example

--- example/examples/NativeWebpage.tsx
+++ example/examples/NativeWebpage.tsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
 
 import WebView from 'react-native-webview';
 
@@ -11,19 +10,7 @@ export default class NativeWebpage extends Component<Props, State> {
 
   render() {
     return (
-      <View style={{ height: 400 }}>
-        <WebView
-          source={{ uri: 'https://infinite.red' }}
-          style={{ width: '100%', height: '100%' }}
-          onShouldStartLoadWithRequest={(event) => {
-            console.log("onShouldStartLoadWithRequest", event);
-            return true;
-          }}
-          onLoadStart={(event) => {
-            console.log("onLoadStart", event.nativeEvent);
-          }}
-        />
-      </View>
+      <WebView source={{ uri: 'https://v0-frame-inspector.vercel.app' }} />
     );
   }
 }
