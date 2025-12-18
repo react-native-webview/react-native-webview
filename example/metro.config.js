f6eea58e2ae63d5ea9f2cc410f079d2b612beaf4
Anupriya Verma <54227869+anupriya13@users.noreply.github.com>
2025-12-18 15:05:46 +0530

    fix autolink

--- example/metro.config.js
+++ example/metro.config.js
@@ -1,11 +1,7 @@
-const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+
 const fs = require('fs');
 const path = require('node:path');
-const escape = require('escape-string-regexp');
-const pack = require('../package.json');
-
-const root = path.resolve(__dirname, '..');
-const modules = Object.keys({ ...pack.peerDependencies });
 
 const rnwPath = fs.realpathSync(
   path.resolve(require.resolve('react-native-windows/package.json'), '..'),
@@ -19,39 +15,22 @@ const rnwPath = fs.realpathSync(
  *
  * @type {import('metro-config').MetroConfig}
  */
+
 const config = {
-  watchFolders: [root,
-    //
-  ],
-
-  // We need to make sure that only one version is loaded for peerDependencies
-  // So we block them at the root, and alias them to the versions in example's node_modules
+  //
   resolver: {
-    blocklist: 
-      modules.map(
-        (m) =>
-          new RegExp(`^${escape(path.join(root, 'node_modules', m))}\\/.*$`)
-      ).concat([
-        // This stops "npx @react-native-community/cli run-windows" from causing the metro server to crash if its already running
-        new RegExp(
-          `${path.resolve(__dirname, 'windows').replace(/[/\\]/g, '/')}.*`,
-        ),
-        // This prevents "npx @react-native-community/cli run-windows" from hitting: EBUSY: resource busy or locked, open msbuild.ProjectImports.zip or other files produced by msbuild
-        new RegExp(`${rnwPath}/build/.*`),
-        new RegExp(`${rnwPath}/target/.*`),
-        /.*\.ProjectImports\.zip/,
-      ]),
-
-    extraNodeModules: modules.reduce((acc, name) => {
-      acc[name] = path.join(__dirname, 'node_modules', name);
-      return acc;
-    },
-    {
-      //
-    }
-    ),
+    blockList: [
+      // This stops "npx @react-native-community/cli run-windows" from causing the metro server to crash if its already running
+      new RegExp(
+        `${path.resolve(__dirname, 'windows').replace(/[/\\]/g, '/')}.*`,
+      ),
+      // This prevents "npx @react-native-community/cli run-windows" from hitting: EBUSY: resource busy or locked, open msbuild.ProjectImports.zip or other files produced by msbuild
+      new RegExp(`${rnwPath}/build/.*`),
+      new RegExp(`${rnwPath}/target/.*`),
+      /.*\.ProjectImports\.zip/,
+    ],
+    //
   },
-
   transformer: {
     getTransformOptions: async () => ({
       transform: {
