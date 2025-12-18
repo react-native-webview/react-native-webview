bbd221b8cedc40e953f7ee896ee13efc209b2d14
Anupriya Verma <54227869+anupriya13@users.noreply.github.com>
2025-12-18 15:05:46 +0530

    fix autolink

--- example/windows/ReactNativeWebviewExample/AutolinkedNativeModules.g.cpp
+++ example/windows/ReactNativeWebviewExample/AutolinkedNativeModules.g.cpp
@@ -3,12 +3,16 @@
 #include "pch.h"
 #include "AutolinkedNativeModules.g.h"
 
+// Includes from react-native-webview
+#include <winrt/ReactNativeWebView.h>
+
 namespace winrt::Microsoft::ReactNative
 {
 
 void RegisterAutolinkedNativeModulePackages(winrt::Windows::Foundation::Collections::IVector<winrt::Microsoft::ReactNative::IReactPackageProvider> const& packageProviders)
 { 
-    UNREFERENCED_PARAMETER(packageProviders);
+    // IReactPackageProviders from react-native-webview
+    packageProviders.Append(winrt::ReactNativeWebView::ReactPackageProvider());
 }
 
 }
