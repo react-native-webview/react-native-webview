b722689b1f8eb1cbd85aec9b948674a10e50fdf3
Thibault Malbranche <malbranche.thibault@gmail.com>
2026-03-02 00:58:08 +0100

    Update NativeRNCWebViewModule.ts

--- src/NativeRNCWebViewModule.ts
+++ src/NativeRNCWebViewModule.ts
@@ -3,8 +3,8 @@ import { TurboModuleRegistry } from 'react-native';
 import { Double } from 'react-native/Libraries/Types/CodegenTypes';
 
 export interface Spec extends TurboModule {
-  isFileUploadSupported: () => Promise<boolean>;
-  shouldStartLoadWithLockIdentifier: (shouldStart: boolean, lockIdentifier: Double) => void;
+  isFileUploadSupported(): Promise<boolean>;
+  shouldStartLoadWithLockIdentifier(shouldStart: boolean, lockIdentifier: Double): void;
 }
 
 export default TurboModuleRegistry.getEnforcing<Spec>('RNCWebViewModule');
