24efac7ae0ef743440978276e63677c5e622cdf7
Thibault Malbranche <malbranche.thibault@gmail.com>
2026-07-11 14:45:44 +0200

    refactor: derive shared native props

--- src/RNCWebViewSharedNativeProps.ts
+++ src/RNCWebViewSharedNativeProps.ts
@@ -1,39 +1,7 @@
 import type { NativeProps as AndroidNativeProps } from './RNCWebViewAndroidNativeComponent';
 import type { NativeProps as IOSNativeProps } from './RNCWebViewIOSNativeComponent';
 
-type SharedNativePropName =
-  | 'allowFileAccessFromFileURLs'
-  | 'allowUniversalAccessFromFileURLs'
-  | 'applicationNameForUserAgent'
-  | 'basicAuthCredential'
-  | 'cacheEnabled'
-  | 'hasOnOpenWindowEvent'
-  | 'incognito'
-  | 'injectedJavaScript'
-  | 'injectedJavaScriptBeforeContentLoaded'
-  | 'injectedJavaScriptBeforeContentLoadedForMainFrameOnly'
-  | 'injectedJavaScriptForMainFrameOnly'
-  | 'injectedJavaScriptObject'
-  | 'javaScriptCanOpenWindowsAutomatically'
-  | 'javaScriptEnabled'
-  | 'mediaPlaybackRequiresUserAction'
-  | 'menuItems'
-  | 'messagingEnabled'
-  | 'newSource'
-  | 'onCustomMenuSelection'
-  | 'onHttpError'
-  | 'onLoadingError'
-  | 'onLoadingFinish'
-  | 'onLoadingProgress'
-  | 'onLoadingStart'
-  | 'onMessage'
-  | 'onOpenWindow'
-  | 'onScroll'
-  | 'onShouldStartLoadWithRequest'
-  | 'showsHorizontalScrollIndicator'
-  | 'showsVerticalScrollIndicator'
-  | 'userAgent'
-  | 'webviewDebuggingEnabled';
+type SharedNativePropName = keyof AndroidNativeProps & keyof IOSNativeProps;
 
 export type SharedNativeProps = Pick<AndroidNativeProps, SharedNativePropName>;
 
@@ -43,5 +11,5 @@ type Equal<Left, Right> = [Left] extends [Right] ? ([Right] extends [Left] ? tru
 
 type Assert<Condition extends true> = Condition;
 
-// Fails type-checking if either platform removes a shared prop or changes its type/optionality.
+// Fails type-checking if overlapping platform props differ in type or optionality.
 export type SharedNativePropsContract = Assert<Equal<IOSSharedNativeProps, SharedNativeProps>>;
